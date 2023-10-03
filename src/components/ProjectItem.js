d46455fa8fbcd001048f2912cfa07677d5d90581
Will Cooley <william.cooley@gmail.coom>
2023-10-03 12:13:26 -0400

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techElements = technologies.map((element)=>{
+    // console.log(element)
+    return <span key={element}>{element}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techElements}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,14 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  console.log(ProjectItem)
+  const newProjects = projects.map((element)=>{
+    return <ProjectItem name={element.name} key={element.id} about={element.about} technologies={element.technologies}/>
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{newProjects}</div>
     </div>
   );
 }
